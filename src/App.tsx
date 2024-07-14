870fcb9927c604e5b955d919ace46c6fec6e28f4
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-07-14 21:59:16 +0200

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
 
+  // Renders the editor instance using a React component.
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Block Docs</h1>
+      <BlockNoteView editor={editor} />
+    </div>
   )
 }
 
-export default App
