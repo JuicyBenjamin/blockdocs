18310525f5b083664ec62a139daf92e8ff36f243
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-07-21 13:23:33 +0200

    feat: add header

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <main className="flex flex-col p-4 justify-center">
-      <Outlet />
-    </main>
+    <>
+      <header className="p-4">
+        <Link to="/">
+          <h3>Block Docs</h3>
+        </Link>
+      </header>
+      <main className="flex flex-col p-4 justify-center">
+        <Outlet />
+      </main>
+    </>
   )
 }
 
